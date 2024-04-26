c5970ce165ec40479c117762e64e24e9cafdb0f8
thomasmendez <thomasmendez01@gmail.com>
2024-04-26 15:04:57 -0500

    Refactored test

--- api/database/skills_tools_test.go
+++ api/database/skills_tools_test.go
@@ -13,39 +13,53 @@ func TestGetSkillsTools(t *testing.T) {
 
 	mockDB := &mockDynamoDB{}
 
-	t.Run("Success case: valid query output", func(t *testing.T) {
-		mockOutput := &dynamodb.QueryOutput{
-			Items: []map[string]*dynamodb.AttributeValue{
-				{
-					"personalWebsiteType": {S: aws.String("SkillsTools")},
-					"sortValue":           {S: aws.String("Programming Languages")},
-					"skillsToolsCategory": {S: aws.String("Tools")},
-					"skillsToolsType":     {S: aws.String("Programming Languages")},
-					"skillsToolsList":     {SS: aws.StringSlice([]string{"Go", "Python", "JavaScript", "Java", "Swift", "C#"})},
-				},
+	for _, test := range []struct {
+		label         string
+		mockQueryFunc func(input *dynamodb.QueryInput) (*dynamodb.QueryOutput, error)
+	}{
+		{
+			label: "valid query output",
+			mockQueryFunc: func(input *dynamodb.QueryInput) (*dynamodb.QueryOutput, error) {
+				mockOutput := &dynamodb.QueryOutput{
+					Items: []map[string]*dynamodb.AttributeValue{
+						{
+							"personalWebsiteType": {S: aws.String("SkillsTools")},
+							"sortValue":           {S: aws.String("Programming Languages")},
+							"skillsToolsCategory": {S: aws.String("Tools")},
+							"skillsToolsType":     {S: aws.String("Programming Languages")},
+							"skillsToolsList":     {SS: aws.StringSlice([]string{"Go", "Python", "JavaScript", "Java", "Swift", "C#"})},
+						},
+					},
+				}
+				return mockOutput, nil
 			},
-		}
+		},
+		{
+			label: "query error",
+			mockQueryFunc: func(input *dynamodb.QueryInput) (*dynamodb.QueryOutput, error) {
+				return nil, errors.New("error querying database")
+			},
+		},
+	} {
+		t.Run(test.label, func(t *testing.T) {
+			mockDB.QueryFunc = test.mockQueryFunc
 
-		mockDB.QueryFunc = func(input *dynamodb.QueryInput) (*dynamodb.QueryOutput, error) {
-			return mockOutput, nil
-		}
+			result, err := GetSkillsTools(mockDB)
 
-		result, err := GetSkillsTools(mockDB)
+			if err != nil {
+				assert.Error(t, err)
+				assert.Empty(t, result)
+				assert.Equal(t, "error querying database", err.Error())
+				return
+			}
 
-		assert.NoError(t, err)
-		assert.Len(t, result, 1)
-		assert.Equal(t, "SkillsTools", result[0].PersonalWebsite)
-		assert.Equal(t, "Programming Languages", result[0].SortValue)
-	})
-
-	t.Run("Error case: query error", func(t *testing.T) {
-		mockDB.QueryFunc = func(input *dynamodb.QueryInput) (*dynamodb.QueryOutput, error) {
-			return nil, errors.New("error querying database")
-		}
-
-		result, err := GetSkillsTools(mockDB)
-
-		assert.Error(t, err)
-		assert.Empty(t, result)
-	})
+			assert.NoError(t, err)
+			assert.Len(t, result, 1)
+			assert.Equal(t, "SkillsTools", result[0].PersonalWebsite)
+			assert.Equal(t, "Programming Languages", result[0].SortValue)
+			assert.Equal(t, "Tools", result[0].SkillsToolsCategory)
+			assert.Equal(t, "Programming Languages", result[0].SkillsToolsType)
+			assert.Equal(t, []string{"Go", "Python", "JavaScript", "Java", "Swift", "C#"}, result[0].SkillsToolsList)
+		})
+	}
 }
