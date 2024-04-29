771755659095b92e7193e2c2158958277c828cff
thomasmendez <thomasmendez01@gmail.com>
2024-04-28 21:03:35 -0500

    Added test for work post function

--- api/database/database_mock.go
+++ api/database/database_mock.go
@@ -10,6 +10,8 @@ import (
 type mockDynamoDB struct {
 	dynamodbiface.DynamoDBAPI
 	QueryFunc func(input *dynamodb.QueryInput) (*dynamodb.QueryOutput, error)
+	PutFunc   func(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error)
+	GetFunc   func(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error)
 }
 
 func (m *mockDynamoDB) Query(input *dynamodb.QueryInput) (*dynamodb.QueryOutput, error) {
@@ -17,4 +19,16 @@ func (m *mockDynamoDB) Query(input *dynamodb.QueryInput) (*dynamodb.QueryOutput,
 		return m.QueryFunc(input)
 	}
 	return nil, errors.New("QueryFunc not implemented")
+}
+func (m *mockDynamoDB) PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
+	if m.PutFunc != nil {
+		return m.PutFunc(input)
+	}
+	return nil, errors.New("PutItem not implemented")
+}
+func (m *mockDynamoDB) GetItem(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
+	if m.GetFunc != nil {
+		return m.GetFunc(input)
+	}
+	return nil, errors.New("GetItem not implemented")
 }
--- api/database/work_test.go
+++ api/database/work_test.go
@@ -45,22 +45,7 @@ func TestWorkGet(t *testing.T) {
 				}
 				return mockOutput, nil
 			},
-			expectedWork: []models.Work{
-				{
-					PersonalWebsiteType: "Work",
-					SortValue:           "2020-01-01",
-					JobTitle:            "Software Engineer",
-					Company:             "ABC Inc",
-					Location: models.Location{
-						City:  "New York",
-						State: "NY",
-					},
-					StartDate:      "2020-01-01",
-					EndDate:        "2020-12-31",
-					JobRole:        "Backend Developer",
-					JobDescription: []string{"Developed backend systems", "Optimized database queries"},
-				},
-			},
+			expectedWork:  []models.Work{expectedWork},
 			expectedError: nil,
 		},
 		{
@@ -96,6 +81,100 @@ func TestWorkGet(t *testing.T) {
 				assert.Equal(t, test.expectedWork[i].JobRole, work.JobRole)
 				assert.Equal(t, test.expectedWork[i].JobDescription, work.JobDescription)
 			}
+		})
+	}
+}
+
+var expectedWork = models.Work{
+	PersonalWebsiteType: "Work",
+	SortValue:           "2020-01-01",
+	JobTitle:            "Software Engineer",
+	Company:             "ABC Inc",
+	Location: models.Location{
+		City:  "New York",
+		State: "NY",
+	},
+	StartDate:      "2020-01-01",
+	EndDate:        "2020-12-31",
+	JobRole:        "Backend Developer",
+	JobDescription: []string{"Developed backend systems", "Optimized database queries"},
+}
+
+func TestPostWork(t *testing.T) {
+	mockDB := &mockDynamoDB{}
+	for _, test := range []struct {
+		label         string
+		newWork       models.Work
+		mockPutFunc   func(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error)
+		mockGetFunc   func(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error)
+		expectedWork  models.Work
+		expectedError error
+	}{
+		{
+			label:   "valid query output",
+			newWork: expectedWork,
+			mockPutFunc: func(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
+				return nil, nil
+			},
+			mockGetFunc: func(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
+				mockOutput := &dynamodb.GetItemOutput{
+					Item: map[string]*dynamodb.AttributeValue{
+						"personalWebsiteType": {S: aws.String("Work")},
+						"sortValue":           {S: aws.String("2020-01-01")},
+						"jobTitle":            {S: aws.String("Software Engineer")},
+						"company":             {S: aws.String("ABC Inc")},
+						"location": {
+							M: map[string]*dynamodb.AttributeValue{
+								"city":  {S: aws.String("New York")},
+								"state": {S: aws.String("NY")},
+							},
+						},
+						"startDate":      {S: aws.String("2020-01-01")},
+						"endDate":        {S: aws.String("2020-12-31")},
+						"jobRole":        {S: aws.String("Backend Developer")},
+						"jobDescription": {SS: []*string{aws.String("Developed backend systems"), aws.String("Optimized database queries")}},
+					},
+				}
+				return mockOutput, nil
+			},
+			expectedWork:  expectedWork,
+			expectedError: nil,
+		},
+		{
+			label:   "query error",
+			newWork: expectedWork,
+			mockPutFunc: func(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
+				return nil, nil
+			},
+			mockGetFunc: func(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
+				return nil, errors.New("error getting item from database")
+			},
+		},
+	} {
+		t.Run(test.label, func(t *testing.T) {
+			mockDB.PutFunc = test.mockPutFunc
+			mockDB.GetFunc = test.mockGetFunc
+
+			result, err := PostWork(mockDB, test.newWork)
+
+			if err != nil {
+				assert.Error(t, err)
+				assert.Empty(t, result)
+				assert.Equal(t, "error getting item from database", err.Error())
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, test.expectedWork.PersonalWebsiteType, result.PersonalWebsiteType)
+			assert.Equal(t, test.expectedWork.SortValue, result.SortValue)
+			assert.Equal(t, test.expectedWork.JobTitle, result.JobTitle)
+			assert.Equal(t, test.expectedWork.Company, result.Company)
+			assert.Equal(t, test.expectedWork.Location.City, result.Location.City)
+			assert.Equal(t, test.expectedWork.Location.State, result.Location.State)
+			assert.Equal(t, test.expectedWork.StartDate, result.StartDate)
+			assert.Equal(t, test.expectedWork.EndDate, result.EndDate)
+			assert.Equal(t, test.expectedWork.JobRole, result.JobRole)
+			assert.Equal(t, test.expectedWork.JobDescription, result.JobDescription)
 		})
 	}
 }
