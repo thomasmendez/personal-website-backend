ea1c453ff317e227ca0d1b003164a038981227d6
thomasmendez <thomasmendez01@gmail.com>
2024-05-12 16:38:00 -0500

    More verbose comments to reusable functions

--- api/database/database.go
+++ api/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"reflect"
 
@@ -21,6 +22,17 @@ func NewDatabase(awsSession *session.Session) (database *Database) {
 	return &Database{dynamodb.New(awsSession)}
 }
 
+// unmarshalMapSlice unmarshals the items in a DynamoDB QueryOutput into a slice of structs.
+// It takes a queryOutput representing the result of a DynamoDB query, and a pointer to a slice
+// of structs (slicePtr) where the unmarshalled items will be stored.
+//
+// Example usage:
+//
+//	var skillsTools []models.SkillsTools
+//	err := unmarshalMapSlice(queryOutput, &skillsTools)
+//	if err != nil {
+//	    log.Printf("error in unmarshalling: %v", err)
+//	}
 func unmarshalDynamodbMapSlice(queryOutput dynamodb.QueryOutput, slicePtr interface{}) error {
 	items := queryOutput.Items
 	sliceValue := reflect.ValueOf(slicePtr).Elem()
@@ -29,14 +41,26 @@ func unmarshalDynamodbMapSlice(queryOutput dynamodb.QueryOutput, slicePtr interf
 	for _, item := range items {
 		newItem := reflect.New(elementType).Interface()
 		if err := dynamodbattribute.UnmarshalMap(item, newItem); err != nil {
-			return err
+			return fmt.Errorf("error in deserializing dynamodb item: %v", elementType.Name())
 		}
 		sliceValue.Set(reflect.Append(sliceValue, reflect.ValueOf(newItem).Elem()))
 	}
 	return nil
 }
 
-func GetItem(svc dynamodbiface.DynamoDBAPI, personalWebsiteType string, sortValue string, item interface{}) (err error) {
+// GetItem retrieves an item from DynamoDB based on the provided personalWebsiteType
+// and sortValue. It takes an initialized DynamoDB client (svc), the personalWebsiteType
+// and sortValue to uniquely identify the item, and a pointer to the struct (itemPtr)
+// where the retrieved item will be unmarshalled.
+//
+// Example usage:
+//
+//	var item models.Item
+//	err := GetItem(svc, "type", "sortValue", &item)
+//	if err != nil {
+//	    log.Printf("error retrieving item: %v", err)
+//	}
+func GetItem(svc dynamodbiface.DynamoDBAPI, personalWebsiteType string, sortValue string, itemPtr interface{}) (err error) {
 	inputGet := &dynamodb.GetItemInput{
 		Key: map[string]*dynamodb.AttributeValue{
 			"personalWebsiteType": {S: aws.String(personalWebsiteType)},
@@ -49,7 +73,7 @@ func GetItem(svc dynamodbiface.DynamoDBAPI, personalWebsiteType string, sortValu
 		log.Printf("error in DynamoDB GetItem func: %v", err)
 		return err
 	}
-	err = dynamodbattribute.UnmarshalMap(result.Item, item)
+	err = dynamodbattribute.UnmarshalMap(result.Item, itemPtr)
 	if err != nil {
 		log.Printf("error in DynamoDB UnmarshalMap func: %v", err)
 		return err
